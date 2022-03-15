0c1f1884b306e75c08cc1a1e44c0697deffeca27
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-03-15 10:23:53 -0400

    commit tree b9e7de9d449102e6f95eb4cdfc94360a19b6f7fc

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map do |a|
+        badge_maker(a)
+    end
+end
+
+def assign_rooms array
+    array.map.with_index(1) do |a, index|
+            "Hello, #{a}! You'll be assigned to room #{index}!"
+        end
+end
+
+def printer array
+    batch_badge_creator(array).each do |a|
+        puts a
+    end
+
+    assign_rooms(array).each do |a|
+        puts a
+    end
+end 
